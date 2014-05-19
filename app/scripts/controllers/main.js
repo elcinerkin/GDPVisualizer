2b0aa87074ab2e9444fc7ea611f9733ab42a0b88
Elcin Erkin <elcinerkin@gmail.com>
2014-05-19 16:51:52 -0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('friendFinderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
