6a92cf42b0bc51804ec9c6b98a7681bd4b65dd7e
Elcin Erkin <elcinerkin@gmail.com>
2014-05-20 01:20:22 -0700

    implements graph

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,78 @@
 'use strict';
 
-angular.module('friendFinderApp', [
+var app = angular.module('friendFinderApp', [
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
-    //,'d3'
+    'ngRoute',
+    'nvd3ChartDirectives'
   ])
 
-.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/main.html',
-      controller: 'someData'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
-});
+//angular.module("nvd3TestApp", ['nvd3ChartDirectives']);
+.controller('ExampleCtrl', function($scope){
+
+  var getData = function(groups, points) {
+    var data = [],
+            shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
+            random = d3.random.normal();
+
+    for (var i = 0; i < groups; i++) {
+        data.push({
+            key: 'Group ' + i,
+            values: []
+        });
+
+        for (var j = 0; j < points; j++) {
+            data[i].values.push({
+            x: random()
+            , y: random()
+            , size: Math.random()
+            //, shape: shapes[j % 6]
+            });
+        }
+    }
+        return data;
+    }
+
+    $scope.exampleData =  getData(4, 40);
+
+    $scope.tooltipXContentFunction = function(){
+        return function(key, x, y) {
+            return '<strong>YO!' + x + '</strong>'
+        }
+    }
+
+    $scope.getShapeCross = function(){
+        return function(d){
+            return 'cross';
+        }
+    }
+
+    $scope.getShapeDiamond = function(){
+        return function(d){
+            return 'diamond';
+        }
+    }
+
+    $scope.getShapeDiamond = function(){
+        return function(d){
+            return 'diamond';
+        }
+    }
+
+})
+
+
+// app.config(function ($routeProvider) {
+//   $routeProvider
+//     .when('/', {
+//       templateUrl: 'views/main.html',
+//       controller: 'someData'
+//     })
+//     .otherwise({
+//       redirectTo: '/'
+//     });
+// });
 
 
 
