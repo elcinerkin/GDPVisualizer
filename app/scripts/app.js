40cdffab847ad55fc8499924b654caa21d130ed6
Elcin Erkin <elcinerkin@gmail.com>
2014-05-19 22:25:02 -0700

    implements d3 scatter plot

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,26 @@
 'use strict';
 
-angular
-  .module('friendFinderApp', [
+angular.module('friendFinderApp', [
     'ngCookies',
     'ngResource',
     'ngSanitize',
     'ngRoute'
+    //,'d3'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'someData'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+
+
+
+//x-y ekseninde uluslarin GDP, Population, GDP per capita grafigi. 
+//data visualization X: Population, y GDP per capita, 
+//dairenin buyuklugu: GDP (bence bu daire yaricapi, r'yi GDP'ye gore normalize ederek yapilabilir)
