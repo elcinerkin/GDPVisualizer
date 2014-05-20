5d0c840ddbda4bb5f38845126e304e6126afa870
Elcin Erkin <elcinerkin@gmail.com>
2014-05-20 16:08:58 -0700

    implemented scope apply

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,97 +8,40 @@ var app = angular.module('friendFinderApp', [
     'nvd3ChartDirectives'
   ])
 
-//angular.module("nvd3TestApp", ['nvd3ChartDirectives']);
 .controller('ExampleCtrl', function($scope){
 
-  var getData = function(groups, points) {
-    var data = [];
-   var shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'];
-    var random = d3.random.normal();
+  var getData = function(groups, points, cb) {
 
-    for (var i = 0; i < groups; i++) {
-        data.push({
-            key: 'Group ' + i,
-            values: []
+    d3.csv('CountryGDP.csv', function(err, data){
+       var csvData = [];
+      if (data) {
+        csvData.push({ 
+          key:'Group 0',
+          values: []
         });
-
-        for (var j = 0; j < points; j++) {
-            data[i].values.push({
-            x: random()
-            , y: random()
-            , size: Math.random()
-            //, shape: shapes[j % 6]
-            });
-        }
-    }
-   // return data;
-   console.log('data 1', data);
-
-  d3.csv('CountryGDP.csv', function(err, data){
-    var csvData = [];
-    //console.log('data', data);
-    csvData.push({ 
-      key:'Group 0',
-      values: []
-    });
-    data.forEach(function(item, index){
-      csvData[0].values.push({
-        x:+item.Population, 
-        y:+item.GDPpercapita,
-        size:Math.random()
-      })
-    //console.log("gdps", item.GDP);
-    });
-    console.log('data 2', csvData);
-    return csvData;
-  });
-
-
-
- }
-
-  $scope.exampleData =  getData(3, 40);
-
-  $scope.tooltipXContentFunction = function(){
-      return function(key, x, y) {
-          return '<strong>YO!' + x + '</strong>'
+        data.forEach(function(item, index){
+          //var linearScale = d3.scale.linear().domain([0,1000000000]).range([0,100]);
+          csvData[0].values.push({
+              x:+item.Population, 
+              y:+item.GDPpercapita,
+              size:Math.random()
+          })
+        });
+        $scope.$apply(function(){
+          cb(csvData);
+        });
       }
+      else{
+        console.log('Error', err);
+      }
+    });  
   }
+
+  getData(1, 40, function(data){$scope.exampleData = data;});
 
   $scope.getShapeCircle = function(){
       return function(d){
           return 'circle';
       }
   }
-
-  $scope.getShapeDiamond = function(){
-      return function(d){
-          return 'diamond';
-      }
-  }
-
-  $scope.getShapeDiamond = function(){
-      return function(d){
-          return 'diamond';
-      }
-  }
-
 })
-
-
-// app.config(function ($routeProvider) {
-//   $routeProvider
-//     .when('/', {
-//       templateUrl: 'views/main.html',
-//       controller: 'someData'
-//     })
-//     .otherwise({
-//       redirectTo: '/'
-//     });
-// });
-
-
-
-//x-y ekseninde uluslarin GDP, Population, GDP per capita grafigi. 
-//data visualization X: Population, y GDP per capita, 
-//dairenin buyuklugu: GDP (bence bu daire yaricapi, r'yi GDP'ye gore normalize ederek yapilabilir)
