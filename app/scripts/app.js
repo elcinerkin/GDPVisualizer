d3884eba2d487e6e157904c141579f4e0f9b8a9f
Elcin Erkin <elcinerkin@gmail.com>
2014-05-20 11:59:32 -0700

    implements mvp

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,9 +12,9 @@ var app = angular.module('friendFinderApp', [
 .controller('ExampleCtrl', function($scope){
 
   var getData = function(groups, points) {
-    var data = [],
-            shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
-            random = d3.random.normal();
+    var data = [];
+   var shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'];
+    var random = d3.random.normal();
 
     for (var i = 0; i < groups; i++) {
         data.push({
@@ -31,34 +31,57 @@ var app = angular.module('friendFinderApp', [
             });
         }
     }
-        return data;
-    }
+   // return data;
+   console.log('data 1', data);
 
-    $scope.exampleData =  getData(4, 40);
+  d3.csv('CountryGDP.csv', function(err, data){
+    var csvData = [];
+    //console.log('data', data);
+    csvData.push({ 
+      key:'Group 0',
+      values: []
+    });
+    data.forEach(function(item, index){
+      csvData[0].values.push({
+        x:+item.Population, 
+        y:+item.GDPpercapita,
+        size:Math.random()
+      })
+    //console.log("gdps", item.GDP);
+    });
+    console.log('data 2', csvData);
+    return csvData;
+  });
 
-    $scope.tooltipXContentFunction = function(){
-        return function(key, x, y) {
-            return '<strong>YO!' + x + '</strong>'
-        }
-    }
 
-    $scope.getShapeCross = function(){
-        return function(d){
-            return 'cross';
-        }
-    }
 
-    $scope.getShapeDiamond = function(){
-        return function(d){
-            return 'diamond';
-        }
-    }
+ }
 
-    $scope.getShapeDiamond = function(){
-        return function(d){
-            return 'diamond';
-        }
-    }
+  $scope.exampleData =  getData(3, 40);
+
+  $scope.tooltipXContentFunction = function(){
+      return function(key, x, y) {
+          return '<strong>YO!' + x + '</strong>'
+      }
+  }
+
+  $scope.getShapeCircle = function(){
+      return function(d){
+          return 'circle';
+      }
+  }
+
+  $scope.getShapeDiamond = function(){
+      return function(d){
+          return 'diamond';
+      }
+  }
+
+  $scope.getShapeDiamond = function(){
+      return function(d){
+          return 'diamond';
+      }
+  }
 
 })
 
